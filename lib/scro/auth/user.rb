e59a6da07e706c69c083af9df1e9d347d28db418
Corey Donohoe <atmos@atmos.org>
2010-07-25 15:09:19 -0700

    pulling down public keys doesn't work either

--- lib/scro/auth/user.rb
+++ lib/scro/auth/user.rb
@@ -12,20 +12,24 @@ module Scro
         info[key]
       end
 
+      def ssh_keys
+        @ssh_keys ||= get("user/keys")["public_keys"]
+      end
+
       def repos
-        @repos ||= get("repos/show/#{@login}")['repositories']
+        @repos ||= get("repos/show/#{@login}")["repositories"]
       end
 
       def watched_repos
-        @watched_repos ||= get("repos/watched/#{@login}")['repositories']
+        @watched_repos ||= get("repos/watched/#{@login}")["repositories"]
       end
 
       def pushable_repos
-        @pushable_repos ||= get("repos/pushable")['repositories']
+        @pushable_repos ||= get("repos/pushable")["repositories"]
       end
 
       def info
-        @info ||= get("user/show/#{@login}")['user']
+        @info ||= get("user/show/#{@login}")["user"]
       end
     end
   end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,16 +1,20 @@
 require "spec_helper"
 
 describe "User" do
-  let(:user) { Scro::Auth::User.new(ENV['SCROAUTH_TOKEN'], 'atmos') }
+  let(:user) { Scro::Auth::User.new(ENV["SCROAUTH_TOKEN"], "atmos") }
 
   it "has simple accessors" do
-    user['name'].should eql('Corey Donohoe')
-    user['blog'].should eql('http://www.atmos.org')
-    user['login'].should eql('atmos')
+    user["name"].should eql("Corey Donohoe")
+    user["blog"].should eql("http://www.atmos.org")
+    user["login"].should eql("atmos")
   end
 
   it "gets your repos" do
     user.repos.size.should be > 50
+  end
+
+  it "gets your ssh keys" do
+    user.ssh_keys.size.should_not be_empty
   end
 
   it "gets watched repos" do
