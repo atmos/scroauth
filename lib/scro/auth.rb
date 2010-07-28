6539746e8e63fd60c99ed6a441b596f9e37b6b64
rick <technoweenie@gmail.com>
2010-07-28 14:11:29 -0700

    restclient responses aren't strings, but they can be if you ask nice

--- lib/scro/auth.rb
+++ lib/scro/auth.rb
@@ -20,14 +20,14 @@ module Scro
       def get(path)
         response = RestClient.get(uri_for(path), {:accept => :json})
         raise RequestError, response.inspect unless response.code == 200
-        JSON.parse(response)
+        JSON.parse(response.to_s)
       end
 
       def post(path, params)
         params.merge!(:access_token => @token)
         response = RestClient.post(uri_for(path), {:params => params, :body => params}, {:content_type => :json, :accept => :json})
         raise RequestError, response.inspect unless response.code == 200
-        JSON.parse(response)
+        JSON.parse(response.to_s)
       end
 
       def uri_for(path)
